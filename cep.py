b8e988218cb25ae4c73ddc65fffd930b0b18c77a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-08 16:01:28 -0300

    try catch para verificar errp

--- cep.py
+++ cep.py
@@ -4,20 +4,23 @@ def main():
     while True:
         cep = input("Digite o CEP que deseja consultar: ")
         if len(cep) == 8 and cep.isdigit():
-            url = f"https://viacep.com.br/ws/{cep}/json"
-            response_json = json.loads(requests.get(url).text)
-            if len(response_json) == 1:
-                print(f"CEP: {cep} não existe, informe outro.")
-                continue
-            else:
-                logradouro = response_json['logradouro']
-                complemento = response_json['complemento']
-                bairro = response_json['bairro']
-                localidade = response_json['localidade']
-                estado = response_json['uf']
-                print(f"Nome da rua: {logradouro} - Complemento: {complemento} - Bairro: {bairro}")
-                print(f"Localidade: {localidade} - Estado: {estado}")
-            break
+            try:
+                url = f"https://viacep.com.br/ws/{cep}/json"
+                response_json = json.loads(requests.get(url).text)
+                if len(response_json) == 1:
+                    print(f"CEP: {cep} não existe, informe outro.")
+                    continue
+                else:
+                    logradouro = response_json['logradouro']
+                    complemento = response_json['complemento']
+                    bairro = response_json['bairro']
+                    localidade = response_json['localidade']
+                    estado = response_json['uf']
+                    print(f"Nome da rua: {logradouro} - Complemento: {complemento} - Bairro: {bairro}")
+                    print(f"Localidade: {localidade} - Estado: {estado}")
+                break
+            except requests.exceptions.RequestException as e:
+                print(f"Erro ao consultar o CEP: {e}")
         else:
             print("O CEP deve conter 8 dígitos")
 
