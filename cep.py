2799b8d3d6b9c7b91bc0e607cfee0dbaaea2dc99
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-08 15:27:26 -0300

    fazendo requisição e trazendo resposta

--- cep.py
+++ cep.py
@@ -4,7 +4,8 @@ def main():
     while True:
         escolha_usuario = input("Digite o CEP que deseja consultar: ")
         if len(escolha_usuario) == 8:
-            print(escolha_usuario)
+            response = requests.get(f"https://viacep.com.br/ws/{escolha_usuario}/json").text
+            print(response)
             break
         else:
             print("O CEP deve conter 8 dígitos")
